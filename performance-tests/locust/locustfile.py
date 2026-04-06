617b6d774a912d37b96dd929034aa48d5f443c18
Armando-Jesus <armandorodz23@gmail.com>
2026-04-06 07:30:37 -0400

    refactor

--- performance-tests/locust/locustfile.py
+++ performance-tests/locust/locustfile.py
@@ -2,4 +2,3 @@ from login import AuthOnlyUser
 from get_customers import CustomerReadUser
 from create_customer import CustomerWriteUser
 
-__all__ = ["AuthOnlyUser", "CustomerReadUser", "CustomerWriteUser"]
